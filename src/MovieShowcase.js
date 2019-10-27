e4a02d659c4c68409815b19dc01c56b8a2df4bbd
Tammy Metz <tammy@tammymetz.com>
2019-10-27 20:30:59 +0000

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -4,15 +4,16 @@ import movieData from './data.js'
 
 export default class MovieShowcase extends Component {
 
+
   generateMovieCards = () => {
-    // map over your movieData array and return the correct 
-  }
+    return movieData.map((data, idx) => <MovieCard key={idx} {...data} />);
+  };
 
   render() {
     return (
       <div id="movie-showcase">
         {this.generateMovieCards()}
       </div>
-    )
+    );
   }
 }
